2f1eb1522b896be34a7640a9585972c7dccfc179
Kyle Nagel <nagelfoundation@gmail.com>
2021-04-26 20:49:05 -0500

    authentication complete

--- backend/nagel_foundation_blog/urls.py
+++ backend/nagel_foundation_blog/urls.py
@@ -3,7 +3,9 @@ from django.urls import path, include , re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_jwt.views import obtain_jwt_token
 from routers import router
+
 
 
 urlpatterns = [
@@ -11,7 +13,9 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('api/blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('api/', include((router.urls, 'nagel_foundation_blog'), namespace='nagel_foundation_blog'))
+    path('api/', include((router.urls, 'nagel_foundation_blog'), namespace='nagel_foundation_blog')),
+    path('token-auth/', obtain_jwt_token),
+    path('core/', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
